150b8b5caed2b256363d1198e69da6cd7d511879
zharmedia386 <muhammadazharalauddin@gmail.com>
2021-08-22 12:13:13 +0700

    Menampilkan data API ke web server lokal

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask.templating import render_template
+from models import ambilData
 
 application = Flask(__name__)
 @application.route('/')
 def index() :
-  return
+  models = ambilData()
+  return render_template('index.html',models=models)
 
 if __name__ == '__main__' :
   application.run(debug=True)
--- models.py
+++ models.py
@@ -1,14 +1,21 @@
 import json
 from urllib import request
 
-url = "https://indonesia-covid-19.mathdro.id/api/provinsi"
+def ambilData() :
+  url = "https://indonesia-covid-19.mathdro.id/api/provinsi"
 
-response = request.urlopen(url);
-data = json.loads(response.read())
+  response = request.urlopen(url);
+  data = json.loads(response.read())
+  return data
 
-for covid in data['data'] :
-  print('------------')
-  print(f"Provinsi : {covid['provinsi']}")
-  print(f"Kasus Positif : {covid['kasusPosi']}")
-  print(f"Kasus Sembuh : {covid['kasusSemb']}")
-  print(f"Kasus Meninggal : {covid['kasusMeni']}")
+  provinsi = data['data']['provinsi']
+  positif = data['data']['kasusPosi']
+  sembuh = data['data']['kasusSemb']
+  meninggal = data['data']['kasusMeni']
+
+  # for covid in data['data'] :
+  #   print('------------')
+  #   print(f"Provinsi : {covid['provinsi']}")
+  #   print(f"Kasus Positif : {covid['kasusPosi']}")
+  #   print(f"Kasus Sembuh : {covid['kasusSemb']}")
+  #   print(f"Kasus Meninggal : {covid['kasusMeni']}")
