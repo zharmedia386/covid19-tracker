4fd23229fa69a51aed848e8109bb1ecd01eebcfc
zharmedia386 <muhammadazharalauddin@gmail.com>
2021-08-24 16:27:46 +0700

    India,Turki,Harian Indo,Global,Tampilan Home,Edit Navbar

--- app.py
+++ app.py
@@ -1,17 +1,52 @@
 from flask import Flask
 from flask.templating import render_template
-from models import ambilData
+from models import provinsi,ambilTanggal,dunia,indonesia,harian_indo,india,india_global,turki,turki_global
 
 application = Flask(__name__)
 @application.route('/')
 def index() :
-  models = ambilData()
-  return render_template('light.html',models=models)
+  return render_template('index.html')
 
-@application.route('/dark')
-def dark():   
-   models = ambilData()
-   return render_template('dark.html',models=models)
+@application.route('/post')
+def post() :
+  return render_template('post.html')
+
+@application.route('/post1')
+def post1():   
+   models = provinsi()
+   date = ambilTanggal()
+   indo = indonesia()
+   return render_template('post1.html',models=models,date=date,indo=indo)
+
+@application.route('/post2')
+def post2():   
+   models = dunia()
+   date = ambilTanggal()
+   length = len(models)
+   return render_template('post2.html',models=models,date=date,length=length)
+
+@application.route('/post3')
+def post3():   
+   models = harian_indo()
+   date = ambilTanggal()
+   length = len(models)
+   return render_template('post3.html',models=models,date=date,length=length)
+
+@application.route('/post4')
+def post4():   
+   models = india()
+   india_ = india_global()
+   date = ambilTanggal()
+   length = len(models)
+   return render_template('post4.html',models=models,date=date,length=length,india=india_)
+
+@application.route('/post5')
+def post5():   
+   models = turki()
+   turki_ = turki_global()
+   date = ambilTanggal()
+   length = len(models)
+   return render_template('post5.html',models=models,date=date,length=length,turki=turki_)
 
 if __name__ == '__main__' :
   application.run(debug=True)
--- models.py
+++ models.py
@@ -1,12 +1,56 @@
 import json
 from urllib import request
+from datetime import datetime
+import requests
 
-def ambilData() :
+def provinsi() :
   url = "https://indonesia-covid-19.mathdro.id/api/provinsi"
 
   response = request.urlopen(url);
   data = json.loads(response.read())
   return data
+
+def dunia() :
+  url = "https://covid19.mathdro.id/api/confirmed"
+
+  response = request.urlopen(url);
+  data = json.loads(response.read())
+  return data
+
+def ambilTanggal() :
+  today = datetime.now().strftime('%A, %d %B %Y')
+  return today
+
+def indonesia() : 
+  url = "https://api.kawalcorona.com/indonesia/"
+
+  response = requests.get(url).json()
+  return response
+
+def harian_indo() :
+  url = "https://apicovid19indonesia-v2.vercel.app/api/indonesia/harian"
+  response = requests.get(url).json()
+  return response
+
+def india() :
+  url = "https://covid19.mathdro.id/api/countries/india/confirmed"
+  response = requests.get(url).json()
+  return response
+
+def india_global() :
+  url = "https://covid19.mathdro.id/api/countries/india"
+  response = requests.get(url).json()
+  return response
+
+def turki() :
+  url = "https://covid19.mathdro.id/api/countries/turkey/confirmed"
+  response = requests.get(url).json()
+  return response
+
+def turki_global() :
+  url = "https://covid19.mathdro.id/api/countries/turkey"
+  response = requests.get(url).json()
+  return response
 
   # provinsi = data['data']['provinsi']
   # positif = data['data']['kasusPosi']
@@ -19,3 +63,5 @@ def ambilData() :
   #   print(f"Kasus Positif : {covid['kasusPosi']}")
   #   print(f"Kasus Sembuh : {covid['kasusSemb']}")
   #   print(f"Kasus Meninggal : {covid['kasusMeni']}")
+
+  # Kawal Covid : covid[iterator]['attributes']['Positif']
